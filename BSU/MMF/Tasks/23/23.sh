90d70d734ca4b95a7132c66776a1b1e344bc8e93
Ivan <2014ivanee@gmail.com>
2017-05-14 18:33:19 +0300

    Corrected syntax in 23.sh

--- BSU/MMF/Tasks/23/23.sh
+++ BSU/MMF/Tasks/23/23.sh
@@ -4,13 +4,13 @@ echo Введите число
 read number
 devider=2
 second=1
-while [ $devider -lt $(($number/2))]; do
+while [ $devider -lt $(($number/2)) ]; do
 	let "second=number%devider"
-	if[ $second -eq 0]; then
+	if [ $second -eq 0 ]; then
 		break
 	fi
 done
-if[$second -eq 0]; then
+if [ $second -eq 0 ]; then
 	echo Простое
 else
 	echo Не простое
